1b5434e5c5d9a7259c613ca6a1a8d7bbba882533
Vitor Zolim <vitorzolim27@hotmail.com>
2026-05-15 16:28:38 -0300

    BaseDecimal Finalizada

--- Bases/BaseDecimal.cs
+++ Bases/BaseDecimal.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Conversor.Engine;
+using System.Text;
+
 namespace Conversor.Bases
 {
     internal class BaseDecimal : INumberBases
@@ -7,7 +9,18 @@ namespace Conversor.Bases
 
         public string ConvertBinary(string value)
         {
-            throw new NotImplementedException();
+            ulong value2 = ulong.Parse(value);
+            StringBuilder sr = new StringBuilder();
+            do
+            {
+                sr.Append(value2 % 2);
+                value2 /= 2;
+            } while (value2 > 0);
+
+            char[] invert = sr.ToString().ToCharArray();
+            Array.Reverse(invert);
+
+            return invert.ToString();
         }
 
         public string ConvertDecimal(string value)
@@ -17,17 +30,58 @@ namespace Conversor.Bases
 
         public string ConvertFifth(string value)
         {
-            throw new NotImplementedException();
+            ulong value2 = ulong.Parse(value);
+            StringBuilder sr = new StringBuilder();
+            do
+            {
+                sr.Append(value2 % 5);
+                value2 /= 5;
+            } while (value2 > 0);
+
+            char[] invert = sr.ToString().ToCharArray();
+            Array.Reverse(invert);
+
+            return invert.ToString();
         }
 
         public string ConvertHexadecimal(string value)
         {
-            throw new NotImplementedException();
+            ulong value2 = ulong.Parse(value);
+            StringBuilder sr = new StringBuilder();
+            do
+            {
+                int x = (int)(value2 % 16);
+                if (x >= 10)
+                {
+                    sr.Append(BaseSelect.ConvertHex(x));
+                }
+                else
+                {
+                    sr.Append(x);
+                }
+                value2 /= 16;
+            }while (value2 > 0);
+
+            char[] invert = sr.ToString().ToCharArray();
+            Array.Reverse(invert);
+
+            return invert.ToString();
         }
 
         public string ConvertOctal(string value)
         {
-            throw new NotImplementedException();
+            ulong value2 = ulong.Parse(value);
+            StringBuilder sr = new StringBuilder();
+            do
+            {
+                sr.Append(value2 % 8);
+                value2 /= 8;
+            } while (value2 > 0);
+
+            char[] invert = sr.ToString().ToCharArray();
+            Array.Reverse(invert);
+
+            return invert.ToString();
         }
     }
 }
